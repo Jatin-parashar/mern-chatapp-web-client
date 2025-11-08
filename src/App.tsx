7700981e99147143ffc5878735be121cd1d718ef
Jatin Parashar <parashar18jatin@gmail.com>
2025-11-08 17:18:49 +0530

    Update App.tsx and main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
-import './App.css'
-import { SocketContextProvider } from './socket/SocketContext'
+import './App.css';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router';
+import { useSelector } from 'react-redux';
+import type { RootState } from './app/store';
+import { SocketContextProvider } from './socket/SocketContext';
+
+// Placeholder components - to be created
+const LoginPage = () => <div>Login Page - To be implemented</div>;
+const RegisterPage = () => <div>Register Page - To be implemented</div>;
+const ChatPage = () => <div>Chat Page - To be implemented</div>;
+const NotFoundPage = () => <div>404 - Page Not Found</div>;
+const AuthBootstrap = () => null; // Auth initialization component
 
 function App() {
+  const accessToken = useSelector((state: RootState) => state.auth.accessToken);
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: accessToken ? (
+        <SocketContextProvider>
+          <ChatPage />
+        </SocketContextProvider>
+      ) : (
+        <Navigate to="/login" />
+      ),
+    },
+    {
+      path: '/login',
+      element: accessToken ? <Navigate to="/" /> : <LoginPage />,
+    },
+    {
+      path: '/register',
+      element: accessToken ? <Navigate to="/" /> : <RegisterPage />,
+    },
+    {
+      path: '*',
+      element: <NotFoundPage />,
+    },
+  ]);
+
   return (
-    <SocketContextProvider>
-      <div className="app">
-        <h1>Chat App</h1>
-        <p>Ready for UI development!</p>
-      </div>
-    </SocketContextProvider>
-  )
+    <>
+      <AuthBootstrap />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { store, persistor } from './app/store'
-import './index.css'
-import App from './App.tsx'
+import { Buffer } from "buffer";
+import process from "process";
+
+window.Buffer = Buffer;
+window.process = process;
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { persistor, store } from "./app/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
